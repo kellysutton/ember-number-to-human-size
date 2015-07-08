5c1a90ddc8367c4fa16b7081e1075cc3a6c07330
kellysutton <michael.k.sutton@gmail.com>
2015-07-08 11:08:26 -0700

    Fix a few things with the dummy app

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/ember-number-to-human-size',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
@@ -40,7 +40,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    ENV.baseURL = '/ember-number-to-human-size';
   }
 
   return ENV;
